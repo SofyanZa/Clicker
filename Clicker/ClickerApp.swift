89d5222292e3c3c5e221bae7241bd82085ee1654
SofyanZa <soza@live.fr>
2021-11-18 11:12:20 +0100

    Initial Commit

---
+++ Clicker/ClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickerApp.swift
+//  Clicker
+//
+//  Created by SofyanZ on 18/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct ClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Clicker
+//
+//  Created by SofyanZ on 18/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
